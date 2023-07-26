952d6d8e20b3ed0ea540938dafba9da2e0289127
maidangkhanh <mai.d.khanh@gmail.com>
2023-07-26 16:14:38 -0700

    Movement

--- Assets/Scripts/Hero.cs
+++ Assets/Scripts/Hero.cs
@@ -1,18 +1,80 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Hero : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    
+    public Animator baseAnim;
+    public Rigidbody body;
+    public SpriteRenderer shadowSprite;
+    
+    [SerializeField] float speed = 2;
+    [SerializeField] float walkSpeed = 2;
+    
+    Vector3 currentDir;
+    bool isFacingLeft;
+    protected Vector3 frontVector;
+
+    void FixedUpdate()
     {
+        Vector3 moveVector = currentDir * speed;
+        body.MovePosition(transform.position + moveVector *
+        Time.fixedDeltaTime);
+        baseAnim.SetFloat("Speed", moveVector.magnitude);
         
+        if (moveVector != Vector3.zero)
+        {
+            if (moveVector.x != 0)
+            {
+                isFacingLeft = moveVector.x < 0;
+            }
+            FlipSprite(isFacingLeft);
+        }
+    }
+    
+    public void FlipSprite(bool isFacingLeft)
+    {
+        if (isFacingLeft)
+        {
+            frontVector = new Vector3(-1, 0, 0);
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else
+        {
+            frontVector = new Vector3(1, 0, 0);
+            transform.localScale = new Vector3(1, 1, 1);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnMove(InputValue value)
     {
-        
+        float h = value.Get<Vector2>().x;
+        float v = value.Get<Vector2>().y;
+
+        currentDir = new Vector3(h, 0, v);
+        currentDir.Normalize();
+
+        if ((v == 0 && h == 0))
+        {
+            Stop();
+        }
+        else if ((v != 0 || h != 0))
+        {
+            Walk();
+        }
+    }
+
+    public void Stop()
+    {
+        speed = 0;
+        baseAnim.SetFloat("Speed", speed);
+    }
+    
+    public void Walk()
+    {
+        speed = walkSpeed;
+        baseAnim.SetFloat("Speed", speed);
     }
 }
