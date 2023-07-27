0e61a932bc33d0c56545cd79cd7420d49ae7731a
maidangkhanh <mai.d.khanh@gmail.com>
2023-07-26 21:55:36 -0700

    running

--- Assets/Scripts/Hero/Hero.cs
+++ Assets/Scripts/Hero/Hero.cs
@@ -12,10 +12,18 @@ public class Hero : MonoBehaviour
     
     [SerializeField] float speed = 2;
     [SerializeField] float walkSpeed = 2;
-    
+    [SerializeField] float runSpeed = 5;
+
     Vector3 currentDir;
     bool isFacingLeft;
     protected Vector3 frontVector;
+
+    bool isRunning;
+    bool isMoving;
+    float lastWalk;
+    public bool canRun = true;
+    float tapAgainToRunTime = 0.2f;
+    Vector3 lastWalkVector;
 
     void FixedUpdate()
     {
@@ -59,22 +67,50 @@ public class Hero : MonoBehaviour
         if ((v == 0 && h == 0))
         {
             Stop();
+            isMoving = false;
         }
-        else if ((v != 0 || h != 0))
+        else if (!isMoving && (v != 0 || h != 0))
         {
-            Walk();
+            isMoving = true;
+            float dotProduct = Vector3.Dot(currentDir, lastWalkVector);
+            if (canRun && Time.time < lastWalk + tapAgainToRunTime && dotProduct > 0)
+            {
+                Run();
+            }
+            else
+            {
+                Walk();
+
+                if (h != 0)
+                {
+                    lastWalkVector = currentDir;
+                    lastWalk = Time.time;
+                }
+            }
         }
     }
 
     public void Stop()
     {
         speed = 0;
+        isRunning = false;
+        baseAnim.SetBool("IsRunning", isRunning);
         baseAnim.SetFloat("Speed", speed);
     }
     
     public void Walk()
     {
         speed = walkSpeed;
+        isRunning = false;
+        baseAnim.SetBool("IsRunning", isRunning);
+        baseAnim.SetFloat("Speed", speed);
+    }
+
+    public void Run()
+    {
+        speed = runSpeed;
+        isRunning = true;
+        baseAnim.SetBool("IsRunning", isRunning);
         baseAnim.SetFloat("Speed", speed);
     }
 }
