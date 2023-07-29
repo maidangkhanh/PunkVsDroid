24563a17f924e5f23eb1e6121c47f3a5659d813a
maidangkhanh <mai.d.khanh@gmail.com>
2023-07-29 12:52:30 -0700

    Get hit

--- Assets/Scripts/Actor.cs
+++ Assets/Scripts/Actor.cs
@@ -13,6 +13,17 @@ public class Actor : MonoBehaviour
     public float speed = 2;
     protected Vector3 frontVector;
     public bool isGrounded;
+    public bool isAlive = true;
+
+    public float maxLife = 100.0f;
+    public float currentLife = 100.0f;
+
+    protected virtual void Start()
+    {
+        currentLife = maxLife;
+        isAlive = true;
+        baseAnim.SetBool("IsAlive", isAlive);
+    }
 
     public virtual void Update()
     {
@@ -61,11 +72,10 @@ public class Actor : MonoBehaviour
         }
     }
 
-    //1
     public virtual void DidHitObject(Collider collider, Vector3 hitPoint, Vector3 hitVector)
     {
         Actor actor = collider.GetComponent<Actor>();
-        if (actor != null)
+        if (actor != null && actor.CanBeHit())
         {
             if (collider.attachedRigidbody != null)
             {
@@ -73,9 +83,56 @@ public class Actor : MonoBehaviour
             }
         }
     }
-    //2
+
+    public bool CanBeHit()
+    {
+        return isAlive;
+    }
+
     protected virtual void HitActor(Actor actor, Vector3 hitPoint, Vector3 hitVector)
     {
-        Debug.Log(gameObject.name + " HIT " + actor.gameObject.name);
+        actor.TakeDamage(10, hitVector);
+    }
+
+    public virtual void TakeDamage(float value, Vector3 hitVector)
+    {
+        FlipSprite(hitVector.x > 0);
+        currentLife -= value;
+
+        if (isAlive && currentLife <= 0)
+        {
+            Die();
+        }
+        else
+        {
+            baseAnim.SetTrigger("IsHurt");
+        }
+    }
+
+    protected virtual void Die()
+    {
+        isAlive = false;
+        baseAnim.SetBool("IsAlive", isAlive);
+        StartCoroutine(DeathFlicker());
+    }
+    
+    protected virtual void SetOpacity(float value)
+    {
+        Color color = baseSprite.color;
+        color.a = value;
+        baseSprite.color = color;
+    }
+    
+    private IEnumerator DeathFlicker()
+    {
+        int i = 5;
+        while (i > 0)
+        {
+            SetOpacity(0.5f);
+            yield return new WaitForSeconds(0.1f);
+            SetOpacity(1.0f);
+            yield return new WaitForSeconds(0.1f);
+            i--;
+        }
     }
 }
